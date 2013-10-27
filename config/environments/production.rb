1e6d4336d468d0091d65ef5e5464dfc2571d4cab
Dennis Rodriguez <drodriguez.0117@gmail.com>
2013-10-27 11:29:12 -0400

    xml changes for multiple addresses and gender; updated gringotts url in prod config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ MainStreetStation::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.gringotts_url = 'http://protected-garden-4145.heroku.com/clients/'
+  config.gringotts_url = 'http://protected-garden-4145.herokuapp.com/clients/'
 end
