3ca3cc75298c115f67a566e19f2d3f67747b6d05
Andy Stechishin <andy.stechishin@gmail.com>
2013-10-28 09:43:35 +1100

    Found another!

--- db/migrate/20120921025825_add_contract_id_to_parties.rb
+++ db/migrate/20120921025825_add_contract_id_to_parties.rb
@@ -1,5 +1,5 @@
 class AddContractIdToParties < ActiveRecord::Migration
   def change
-    add_column :parties, :contract_id, :integer
+    add_column :registration_parties, :contract_id, :integer
   end
 end
