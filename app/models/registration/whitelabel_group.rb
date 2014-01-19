623e24ca4ed3f90d9493b2d26218f15a1a2f2844
Andy Stechishin <andy.stechishin@gmail.com>
2014-01-19 16:32:32 -0600

    Removed deprecated attr_accesible

--- app/models/registration/whitelabel_group.rb
+++ app/models/registration/whitelabel_group.rb
@@ -1,5 +1,4 @@
 class Registration::WhitelabelGroup < ActiveRecord::Base
-
   belongs_to :registration_whitelabel, :class_name => 'Registration::Whitelabel'
   belongs_to :directory_community, :class_name => 'Directory::Community'
 end
