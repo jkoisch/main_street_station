9c98560b1277da4ba02ef730e536992b050946c9
Andy Stechishin <andy.stechishin@gmail.com>
2014-02-22 13:11:08 -0700

    Added FHIR API spec directory to test FHIR integration

---
+++ spec/fhir_api/conformance_integration_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "Conformance" do
+  describe "GET /Conformance" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get fhir_conformance_path
+      response.status.should be(200)
+    end
+  end
+
+  describe "GET /metadata" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get fhir_conformance_path
+      response.status.should be(200)
+    end
+  end
+end
