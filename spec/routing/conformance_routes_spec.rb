1a687d7ea71aa9dead98e9ee0009eb7a0f21ccb6
Andy Stechishin <andy.stechishin@gmail.com>
2014-06-14 14:08:09 -0600

    Correct routing, fix some typos

--- spec/routing/conformance_routes_spec.rb
+++ spec/routing/conformance_routes_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe 'FHIR Conformance' do
   describe 'provided routes' do
-    specify { expect(get: '/fhir/metadata').to route_to(controller: 'fhir/conformance',
-                                                        action:     'index',
-                                                        format:     :json) }
-    specify { expect(get: fhir_conformance_path).to route_to(controller: 'fhir/conformance',
-                                                             action:     'index',
-                                                             format:     :json) }
+    specify { expect(get: fhir_conformance_index_path).to route_to(controller: 'fhir/conformance',
+                                                                   action:     'index',
+                                                                   format:     :json) }
     specify { expect(get: '/fhir/Conformance').to route_to(controller: 'fhir/conformance',
                                                            action:     'index',
                                                            format:     :json) }
+    specify { expect(get: '/fhir/metadata').to route_to(controller: 'fhir/conformance',
+                                                        action:     'show',
+                                                        format:     :json) }
     specify { expect(get: fhir_conformance_path(1)).to route_to(controller: 'fhir/conformance',
                                                                 action:     'show',
                                                                 id:         '1',
@@ -26,7 +26,7 @@ describe 'FHIR Conformance' do
     #specify { expect(get: '/fhir/Conformance/new').not_to be_routable }
     specify { expect(post: '/fhir/Conformance').not_to be_routable }
     specify { expect(get: '/fhir/Conformance/edit/1').not_to be_routable }
-    specify { expect(put: '/fhir/Conformance/edit/1').not_to be_routable }
+    specify { expect(put: '/fhir/Conformance/1').not_to be_routable }
     specify { expect(delete: '/fhir/Conformance/1').not_to be_routable }
   end
 end
