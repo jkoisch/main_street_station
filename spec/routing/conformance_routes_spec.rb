03bebf3cc069b071c274424c6cdc1028ceafe356
Dennis Rodriguez <drodriguez.0117@gmail.com>
2014-06-16 20:55:59 -0400

    additional conformance route specs for correct case

--- spec/routing/conformance_routes_spec.rb
+++ spec/routing/conformance_routes_spec.rb
@@ -24,9 +24,15 @@ describe 'FHIR Conformance' do
   describe 'forbidden routes' do
     # the following routes matches the show path, not testable
     #specify { expect(get: '/fhir/Conformance/new').not_to be_routable }
+    specify { expect(get: '/fhir/conformance').not_to be_routable }
+    specify { expect(get: '/fhir/conformance/1').not_to be_routable }
     specify { expect(post: '/fhir/Conformance').not_to be_routable }
+    specify { expect(post: '/fhir/conformance').not_to be_routable }
     specify { expect(get: '/fhir/Conformance/edit/1').not_to be_routable }
+    specify { expect(get: '/fhir/conformance/edit/1').not_to be_routable }
     specify { expect(put: '/fhir/Conformance/1').not_to be_routable }
+    specify { expect(put: '/fhir/conformance/1').not_to be_routable }
     specify { expect(delete: '/fhir/Conformance/1').not_to be_routable }
+    specify { expect(delete: '/fhir/conformance/1').not_to be_routable }
   end
 end
