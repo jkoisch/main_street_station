53fcad5f4ef54cd950118627b6ae98c7ea72fdf0
Dennis Rodriguez <drodriguez.0117@gmail.com>
2014-07-26 20:47:49 -0400

    added operation outcome to create and update - family history controller ran family history controller spec for operation outcome testing

--- app/controllers/fhir/family_histories_controller.rb
+++ app/controllers/fhir/family_histories_controller.rb
@@ -38,10 +38,10 @@ module Fhir
         if response.success?
           render nothing: true, status: 201, location: "#{FHIR_LOCATION_ROOT}/FamilyHistory/#{response.body[:id]}"
         else
-          render text: 'nothing', status: 400
+          send_operation_outcome(response, 400)
         end
       else
-        render text: 'Please try again later', status: 503
+        send_operation_outcome(nil, 503, 'Application unavailable at this time')
       end
     end
 
@@ -51,10 +51,10 @@ module Fhir
         if response.success?
           render nothing: true, status: 200
         else
-          render text: 'nothing', status: 400
+          send_operation_outcome(response, 400)
         end
       else
-        render text: 'Please try again later', status: 503
+        send_operation_outcome(nil, 503, 'Application unavailable at this time')
       end
     end
 
