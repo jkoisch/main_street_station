75c90431e20fa30fe528133090a18e1c2c113ae1
Dennis Rodriguez <drodriguez.0117@gmail.com>
2014-10-01 23:12:00 -0400

    moved fhir operation_outcome support-files to fhir/operation_outcome

--- spec/views/fhir_operation_outcome_view_spec.rb
+++ spec/views/fhir_operation_outcome_view_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 
 describe 'FHIR OperationOutcome View' do
   it 'should produce valid JSON' do
-    @operation_outcome = YAML.load(File.read('spec/support-files/operation-outcome.yaml'))
+    @operation_outcome = YAML.load(File.read('spec/support-files/fhir/operation_outcome/operation-outcome.yaml'))
     response = render template: 'fhir/fhir_base/operation_outcome', formats: :json
-    expected = File.read('spec/support-files/fhir-operation-outcome.json')
+    expected = File.read('spec/support-files/fhir/operation_outcome/fhir-operation-outcome.json')
     expect(response).to eq(expected)
   end
 
   it 'should produce valid XML' do
-    @operation_outcome = YAML.load(File.read('spec/support-files/operation-outcome.yaml'))
+    @operation_outcome = YAML.load(File.read('spec/support-files/fhir/operation_outcome/operation-outcome.yaml'))
     response = render template: 'fhir/fhir_base/operation_outcome', formats: :xml
-    expected = File.read('spec/support-files/fhir-operation-outcome.xml')
+    expected = File.read('spec/support-files/fhir/operation_outcome/fhir-operation-outcome.xml')
     expect(response).to eq(expected)
   end
 end
