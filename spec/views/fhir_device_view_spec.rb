74de1b0691064a2d3341c86806295788cc78c85d
Dennis Rodriguez <drodriguez.0117@gmail.com>
2014-10-01 22:29:44 -0400

    moved fhir device support-files to fhir/device

--- spec/views/fhir_device_view_spec.rb
+++ spec/views/fhir_device_view_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'FHIR Device View' do
   subject { 'fhir/devices/device' }
-  let(:resource) { YAML.load(File.read('spec/support-files/device_general.yaml')) }
+  let(:resource) { YAML.load(File.read('spec/support-files/fhir/device/device_general.yaml')) }
 
-  it {should produce_fhir_json_like('spec/support-files/fhir-device_general.json')}
-  it {should produce_fhir_xml_like('spec/support-files/fhir-device_general.xml')}
+  it {should produce_fhir_json_like('spec/support-files/fhir/device/fhir-device_general.json')}
+  it {should produce_fhir_xml_like('spec/support-files/fhir/device/fhir-device_general.xml')}
 end
