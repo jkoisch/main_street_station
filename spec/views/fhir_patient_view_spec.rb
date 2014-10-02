59e32fe0d3ecfec7fba3d414e66bfa55c2d490d1
Dennis Rodriguez <drodriguez.0117@gmail.com>
2014-10-01 23:21:53 -0400

    moved fhir patient support-files to fhir/patient

--- spec/views/fhir_patient_view_spec.rb
+++ spec/views/fhir_patient_view_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'FHIR Patient View' do
   subject { 'fhir/patients/patient' }
-  let(:resource) { YAML.load(File.read('spec/support-files/patient.yaml')) }
+  let(:resource) { YAML.load(File.read('spec/support-files/fhir/patient/patient.yaml')) }
 
-  it {should produce_fhir_json_like('spec/support-files/fhir-patient.json')}
-  it {should produce_fhir_xml_like('spec/support-files/fhir-patient.xml')}
+  it {should produce_fhir_json_like('spec/support-files/fhir/patient/fhir-patient.json')}
+  it {should produce_fhir_xml_like('spec/support-files/fhir/patient/fhir-patient.xml')}
 end
