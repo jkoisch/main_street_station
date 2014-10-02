200ed2f3b9e82064b21bf3d9f11eb13839094366
Dennis Rodriguez <drodriguez.0117@gmail.com>
2014-10-01 23:25:00 -0400

    moved fhir practitioner support-files to fhir/practitioner

--- spec/views/fhir_practitioner_view_spec.rb
+++ spec/views/fhir_practitioner_view_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'FHIR Practitioner View' do
   subject { 'fhir/practitioners/practitioner' }
-  let(:resource) { YAML.load(File.read('spec/support-files/practitioner.yaml')) }
+  let(:resource) { YAML.load(File.read('spec/support-files/fhir/practitioner/practitioner.yaml')) }
 
-  it {should produce_fhir_json_like('spec/support-files/fhir-practitioner.json')}
-  it {should produce_fhir_xml_like('spec/support-files/fhir-practitioner.xml')}
+  it {should produce_fhir_json_like('spec/support-files/fhir/practitioner/fhir-practitioner.json')}
+  it {should produce_fhir_xml_like('spec/support-files/fhir/practitioner/fhir-practitioner.xml')}
 end
