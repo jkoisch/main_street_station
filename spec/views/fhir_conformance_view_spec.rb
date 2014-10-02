2275a753587a12b4c9252b8869f6ff5876074c3d
Dennis Rodriguez <drodriguez.0117@gmail.com>
2014-10-01 22:26:29 -0400

    moved fhir conformance support-files to fhir/conformance

--- spec/views/fhir_conformance_view_spec.rb
+++ spec/views/fhir_conformance_view_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'FHIR Conformance View' do
   subject { 'fhir/conformance/conformance' }
-  let(:resource) { YAML.load(File.read('spec/support-files/conformance.yaml')) }
+  let(:resource) { YAML.load(File.read('spec/support-files/fhir/conformance/conformance.yaml')) }
 
-  it {should produce_fhir_json_like('spec/support-files/fhir-conformance.json')}
-  it {should produce_fhir_xml_like('spec/support-files/fhir-conformance.xml')}
+  it {should produce_fhir_json_like('spec/support-files/fhir/conformance/fhir-conformance.json')}
+  it {should produce_fhir_xml_like('spec/support-files/fhir/conformance/fhir-conformance.xml')}
 end
