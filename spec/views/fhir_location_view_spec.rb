1fc5ff1f9186a669893023363c500f183888ca7f
Dennis Rodriguez <drodriguez.0117@gmail.com>
2014-10-01 23:03:20 -0400

    moved fhir location support-files to fhir/location

--- spec/views/fhir_location_view_spec.rb
+++ spec/views/fhir_location_view_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'FHIR Location View' do
   subject { 'fhir/locations/location' }
-  let(:resource) { YAML.load(File.read('spec/support-files/location.yaml')) }
+  let(:resource) { YAML.load(File.read('spec/support-files/fhir/location/location.yaml')) }
 
-  it {should produce_fhir_json_like('spec/support-files/fhir-location.json')}
-  it {should produce_fhir_xml_like('spec/support-files/fhir-location.xml')}
+  it {should produce_fhir_json_like('spec/support-files/fhir/location/fhir-location.json')}
+  it {should produce_fhir_xml_like('spec/support-files/fhir/location/fhir-location.xml')}
 end
