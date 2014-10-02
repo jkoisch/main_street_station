a74be343b71aabf4fdb9605f4fc6e53f7095dfb8
Dennis Rodriguez <drodriguez.0117@gmail.com>
2014-10-01 22:21:39 -0400

    moved fhir condition support-files to fhir/condition support-file clean up of unused patient.json

--- spec/views/fhir_condition_view_spec.rb
+++ spec/views/fhir_condition_view_spec.rb
@@ -3,8 +3,8 @@ require 'fhir/condition'
 
 describe 'FHIR Condition View' do
   subject { 'fhir/conditions/condition' }
-  let(:resource) { YAML.load(File.read('spec/support-files/condition.yaml')) }
+  let(:resource) { YAML.load(File.read('spec/support-files/fhir/condition/condition.yaml')) }
 
-  it {should produce_fhir_json_like('spec/support-files/fhir-condition.json')}
-  it {should produce_fhir_xml_like('spec/support-files/fhir-condition.xml')}
+  it {should produce_fhir_json_like('spec/support-files/fhir/condition/fhir-condition.json')}
+  it {should produce_fhir_xml_like('spec/support-files/fhir/condition/fhir-condition.xml')}
 end
