3973c21c8faf32de79f21134c28523e086478662
Dennis Rodriguez <drodriguez.0117@gmail.com>
2014-10-01 23:15:23 -0400

    moved fhir organization support-files to fhir/organization

--- spec/views/fhir_organization_view_spec.rb
+++ spec/views/fhir_organization_view_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'FHIR Organization View' do
   subject { 'fhir/organizations/organization' }
-  let(:resource) { YAML.load(File.read('spec/support-files/organization.yaml')) }
+  let(:resource) { YAML.load(File.read('spec/support-files/fhir/organization/organization.yaml')) }
 
-  it {should produce_fhir_json_like('spec/support-files/fhir-organization.json')}
-  it {should produce_fhir_xml_like('spec/support-files/fhir-organization.xml')}
+  it {should produce_fhir_json_like('spec/support-files/fhir/organization/fhir-organization.json')}
+  it {should produce_fhir_xml_like('spec/support-files/fhir/organization/fhir-organization.xml')}
 end
