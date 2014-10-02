655c53efa9b99e370ad9ede66a18d07567e5da27
Dennis Rodriguez <drodriguez.0117@gmail.com>
2014-10-01 22:59:04 -0400

    moved fhir family_history support-files to fhir/family_history

--- spec/views/fhir_family_history_view_spec.rb
+++ spec/views/fhir_family_history_view_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'FHIR FamilyHistory View' do
   subject { 'fhir/family_histories/family_history' }
-  let(:resource) { YAML.load(File.read('spec/support-files/family_history.yaml')) }
+  let(:resource) { YAML.load(File.read('spec/support-files/fhir/family_history/family_history.yaml')) }
 
-  it {should produce_fhir_json_like('spec/support-files/fhir-family_history.json')}
-  it {should produce_fhir_xml_like('spec/support-files/fhir-family_history.xml')}
+  it {should produce_fhir_json_like('spec/support-files/fhir/family_history/fhir-family_history.json')}
+  it {should produce_fhir_xml_like('spec/support-files/fhir/family_history/fhir-family_history.xml')}
 end
