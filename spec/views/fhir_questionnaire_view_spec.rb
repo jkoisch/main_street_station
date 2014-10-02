69e2a2e077fe6e19cff760635cb52853bd3af1d2
Dennis Rodriguez <drodriguez.0117@gmail.com>
2014-10-01 23:28:36 -0400

    moved fhir questionnaire support-files to fhir/questionnaire

--- spec/views/fhir_questionnaire_view_spec.rb
+++ spec/views/fhir_questionnaire_view_spec.rb
@@ -3,26 +3,26 @@ require 'spec_helper'
 describe 'FHIR Questionnaire View' do
   context 'general questionnaires' do
     subject { 'fhir/questionnaires/questionnaire' }
-    let(:resource) { YAML.load(File.read('spec/support-files/questionnaire_general.yaml'))}
+    let(:resource) { YAML.load(File.read('spec/support-files/fhir/questionnaire/questionnaire_general.yaml'))}
 
-    it {should produce_fhir_json_like('spec/support-files/fhir-questionnaire_general.json')}
-    it {should produce_fhir_xml_like('spec/support-files/fhir-questionnaire_general.xml')}
+    it {should produce_fhir_json_like('spec/support-files/fhir/questionnaire/fhir-questionnaire_general.json')}
+    it {should produce_fhir_xml_like('spec/support-files/fhir/questionnaire/fhir-questionnaire_general.xml')}
   end
 
   context 'lifeline questionnaires' do
     subject { 'fhir/questionnaires/questionnaire' }
-    let(:resource) { YAML.load(File.read('spec/support-files/questionnaire_lifelines.yaml')) }
+    let(:resource) { YAML.load(File.read('spec/support-files/fhir/questionnaire/questionnaire_lifelines.yaml')) }
 
-    it {should produce_fhir_json_like('spec/support-files/fhir-questionnaire_lifelines.json')}
-    it {should produce_fhir_xml_like('spec/support-files/fhir-questionnaire_lifelines.xml')}
+    it {should produce_fhir_json_like('spec/support-files/fhir/questionnaire/fhir-questionnaire_lifelines.json')}
+    it {should produce_fhir_xml_like('spec/support-files/fhir/questionnaire/fhir-questionnaire_lifelines.xml')}
   end
 
 
   context 'bluebook questionnaires' do
     subject { 'fhir/questionnaires/questionnaire' }
-    let(:resource) { YAML.load(File.read('spec/support-files/questionnaire_bluebook.yaml')) }
+    let(:resource) { YAML.load(File.read('spec/support-files/fhir/questionnaire/questionnaire_bluebook.yaml')) }
 
-    it {should produce_fhir_json_like('spec/support-files/fhir-questionnaire_bluebook.json')}
-    it {should produce_fhir_xml_like('spec/support-files/fhir-questionnaire_bluebook.xml')}
+    it {should produce_fhir_json_like('spec/support-files/fhir/questionnaire/fhir-questionnaire_bluebook.json')}
+    it {should produce_fhir_xml_like('spec/support-files/fhir/questionnaire/fhir-questionnaire_bluebook.xml')}
   end
 end
