6a972a043ac24b95a7f816a85ce970beb68e1da9
Dennis Rodriguez <drodriguez.0117@gmail.com>
2014-10-01 23:07:24 -0400

    moved fhir observation support-files to fhir/observation

--- spec/views/fhir_observation_view_spec.rb
+++ spec/views/fhir_observation_view_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 describe 'FHIR Observation Views' do
   context 'general observation' do
     subject { '/fhir/observations/observation' }
-    let(:resource) { YAML.load(File.read('spec/support-files/observation_general.yaml')) }
+    let(:resource) { YAML.load(File.read('spec/support-files/fhir/observation/observation_general.yaml')) }
 
-    it {should produce_fhir_json_like('spec/support-files/fhir-observation_general.json')}
-    it {should produce_fhir_xml_like('spec/support-files/fhir-observation_general.xml')}
+    it {should produce_fhir_json_like('spec/support-files/fhir/observation/fhir-observation_general.json')}
+    it {should produce_fhir_xml_like('spec/support-files/fhir/observation/fhir-observation_general.xml')}
   end
 
   context 'glucose observation' do
     subject { '/fhir/observations/observation' }
-    let(:resource) { YAML.load(File.read('spec/support-files/observation_glucose.yaml')) }
+    let(:resource) { YAML.load(File.read('spec/support-files/fhir/observation/observation_glucose.yaml')) }
 
-    it {should produce_fhir_json_like('spec/support-files/fhir-observation_glucose.json')}
-    it {should produce_fhir_xml_like('spec/support-files/fhir-observation_glucose.xml')}
+    it {should produce_fhir_json_like('spec/support-files/fhir/observation/fhir-observation_glucose.json')}
+    it {should produce_fhir_xml_like('spec/support-files/fhir/observation/fhir-observation_glucose.xml')}
   end
 end
