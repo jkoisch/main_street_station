a01f76fa798dcc60407e9ac89ad72273a8ad0a3d
Dennis Rodriguez <drodriguez.0117@gmail.com>
2015-10-14 22:26:45 -0400

    Conformance Update for FHIR v1.0.0
    Updated Conformance.json config
    Updated Models for ConformanceClasses Operation, Resource, and Search Param
    Updated Views for ConformanceClasses Operation, Resource, and Search Param
    Added tests for required elements for fhir resources
    Updated Conformance support files to include required elements

--- app/models/fhir/conformance_classes/operation.rb
+++ app/models/fhir/conformance_classes/operation.rb
@@ -1,8 +1,8 @@
 module Fhir
   module ConformanceClasses
     class Operation < BaseClass
-      fhir_attribute :code
-      fhir_attribute :documentation
+      fhir_attribute :name
+      fhir_attribute :definition
     end
   end
 end
