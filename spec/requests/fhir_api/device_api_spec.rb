821ae348504011c3497d946fa72afb4c118f0099
Dennis Rodriguez <drodriguez.0117@gmail.com>
2015-10-20 22:23:16 -0400

    XML and JSON API and View Testing of Device
    Added additional tests to the Device API Spec
    Added Device Complete JSON and XML Spec files
    Added JSON and XML spec files for Device Index and Show
    Modified Device View Spec for additional view tests

--- spec/requests/fhir_api/device_api_spec.rb
+++ spec/requests/fhir_api/device_api_spec.rb
@@ -3,7 +3,68 @@ require 'support/fhir_matchers'
 
 describe 'Device Request FHIR API', type: :request do
   describe 'GET /fhir/Device' do
-    it { should return_FHIR_JSON_bundle_object('Device') }
+
+    context 'JSON' do
+      context 'success' do
+        before(:each) { GringottResponse.any_instance.stubs(:success?).returns(true) }
+
+          it 'should return a FHIR Bundle' do
+            get '/fhir/Device'
+            expect(response).to return_FHIR_JSON_bundle('Device')
+          end
+
+          it 'should have success status (2xx)' do
+            get '/fhir/Device'
+            expect(response).to have_http_status(:success)
+          end
+      end
+
+      context 'failure' do
+        before(:each) { GringottResponse.any_instance.stubs(:success?).returns(false) }
+
+        it 'should return error status' do
+          get '/fhir/Device'
+          expect(response).to have_http_status(:error)
+        end
+
+        it 'should render OperationOutcome' do
+          expect(get '/fhir/Device').to render_template('fhir_base/operation_outcome')
+        end
+      end
+    end
+
+    context 'XML' do
+      let(:headers) { {'Accept' => 'application/xml'} }
+
+      context 'success' do
+        before(:each) { GringottResponse.any_instance.stubs(:success?).returns(true) }
+
+        it 'should return a FHIR Bundle' do
+          get 'fhir/Device', {}, headers
+          expect(response).to return_FHIR_XML_bundle('Device')
+        end
+
+        it 'should have success status (2xx)' do
+          get 'fhir/Device', {}, headers
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context 'failure' do
+        before(:each) { GringottResponse.any_instance.stubs(:success?).returns(false) }
+
+        it 'should return error status' do
+          get '/fhir/Device', {}, headers
+          expect(response).to have_http_status(:error)
+        end
+
+        it 'should render OperationOutcome' do
+          get '/fhir/Device', {}, headers
+          expect(response).to render_template('fhir_base/operation_outcome')
+          expect(response.content_type).to eq :xml
+        end
+      end
+    end
 
     it 'should return_HTTP_success_for Device.json' do
       GringottResponse.any_instance.stubs(:success?).returns(true)
@@ -22,8 +83,26 @@ describe 'Device Request FHIR API', type: :request do
   end
 
   describe 'GET /fhir/Device/1.json' do
-    it { should return_FHIR_JSON_object('Device') }
+    context 'JSON' do
+      it { should return_FHIR_JSON_object('Device') }
 
-    it { should return_HTTP_success_for('Device/1.json') }
+      it { should return_HTTP_success_for('Device/1.json') }
+    end
+
+    context 'XML' do
+      let(:headers) { {'Accept' => 'application/xml'} }
+
+      context 'success' do
+        it 'should return a FHIR resource' do
+          get 'fhir/Device/1', {}, headers
+          expect(response).to return_FHIR_XML_object('Device')
+        end
+
+        it 'should have success status (2xx)' do
+          get 'fhir/Device/1', {}, headers
+          expect(response).to have_http_status(:success)
+        end
+      end
+    end
   end
 end
