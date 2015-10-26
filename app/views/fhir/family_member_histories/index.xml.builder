d116975fde68a1a91a757d976b667161c0a663b2
Dennis Rodriguez <drodriguez.0117@gmail.com>
2015-10-25 22:22:09 -0400

    XML and JSON API and View Testing of FamilyMemberHistory
    Added additional tests to the FamilyMemberHistory API Spec
    Added FamilyMemberHistory Complete JSON and XML Spec files
    Added JSON and XML spec files for FamilyMemberHistory Index and Show
    Modified FamilyMemberHistory View Spec for additional view tests
    Updated FamilyMemberHistoryClasses:Condition Model and Views
    Added FamilyMemberHistoryClasses:Condition Complete and Standard JSON and XML Spec files
    Added FamilyMemberHistoryClasses:Condition View specs

--- app/views/fhir/family_member_histories/index.xml.builder
+++ app/views/fhir/family_member_histories/index.xml.builder
@@ -1,9 +1,2 @@
 xml.instruct!
 xml_bundle(xml, @family_member_histories)
-# xml.family_member_histories do
-#   @family_member_histories.each do |family_member_history|
-#     xml.family_member_history do
-#       xml << render(:partial => 'family_member_history', :locals => { :resource => family_member_history })
-#     end
-#   end
-# end
--- spec/requests/fhir_api/device_api_spec.rb
+++ spec/requests/fhir_api/device_api_spec.rb
@@ -65,21 +65,6 @@ describe 'Device Request FHIR API', type: :request do
         end
       end
     end
-
-    it 'should return_HTTP_success_for Device.json' do
-      GringottResponse.any_instance.stubs(:success?).returns(true)
-      expect(get '/fhir/Device.json'). to eq 200
-    end
-
-    it 'should return error for failure' do
-      GringottResponse.any_instance.stubs(:success?).returns(false)
-      expect(get '/fhir/Device.json').to eq 500
-    end
-
-    it 'should render OperationOutcome for a failure' do
-      GringottResponse.any_instance.stubs(:success?).returns(false)
-      expect(get '/fhir/Device.json').to render_template('fhir_base/operation_outcome')
-    end
   end
 
   describe 'GET /fhir/Device/1.json' do
