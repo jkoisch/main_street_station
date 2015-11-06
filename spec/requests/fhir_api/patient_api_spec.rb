4950b1d4db13524582421ca46ff34083022b2925
Dennis Rodriguez <drodriguez.0117@gmail.com>
2015-11-05 22:57:40 -0500

    XML and JSON API and View Testing of Patient
    Added additional tests to the Patient API Spec
    Added Patient Complete JSON and XML Spec files
    Added JSON and XML spec files for Patient Index and Show
    Modified Patient View Spec for additional view tests

--- spec/requests/fhir_api/patient_api_spec.rb
+++ spec/requests/fhir_api/patient_api_spec.rb
@@ -3,29 +3,91 @@ require 'support/fhir_matchers'
 
 describe 'Patient Request FHIR API', type: :request do
   describe 'GET /fhir/Patient' do
-    it { should return_FHIR_JSON_bundle_object('Patient') }
 
-    it 'should return_HTTP_success_for Patient.json' do
-      GringottResponse.any_instance.stubs(:success?).returns(true)
-      expect(get '/fhir/Patient.json'). to eq 200
+    context 'JSON' do
+      context 'success' do
+        before(:each) { GringottResponse.any_instance.stubs(:success?).returns(true) }
+
+        it 'should return a FHIR Bundle' do
+          get '/fhir/Patient'
+          expect(response).to return_FHIR_JSON_bundle('Patient')
+        end
+
+        it 'should have success status (2xx)' do
+          get '/fhir/Patient'
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context 'failure' do
+        before(:each) { GringottResponse.any_instance.stubs(:success?).returns(false) }
+
+        it 'should return error status' do
+          get '/fhir/Patient'
+          expect(response).to have_http_status(:error)
+        end
+
+        it 'should render OperationOutcome' do
+          expect(get 'fhir/Patient').to render_template('fhir_base/operation_outcome')
+        end
+      end
     end
 
-    it 'should return error for failure' do
-      GringottResponse.any_instance.stubs(:success?).returns(false)
-      expect(get '/fhir/Patient.json').to eq 500
-    end
+    context 'XML' do
+      let(:headers) { {'Accept' => 'application/xml'} }
 
-    it 'should render OperationOutcome for a failure' do
-      GringottResponse.any_instance.stubs(:success?).returns(false)
-      expect(get '/fhir/Patient.json').to render_template('fhir_base/operation_outcome')
+      context 'success' do
+        before(:each) { GringottResponse.any_instance.stubs(:success).returns(true) }
+
+        it 'should return a FHIR Bundle' do
+          get 'fhir/Patient', {}, headers
+          expect(response).to return_FHIR_XML_bundle('Patient')
+        end
+
+        it 'should have success status (2xx)' do
+          get 'fhir/Patient', {}, headers
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context 'failure' do
+        before(:each) { GringottResponse.any_instance.stubs(:success?).returns(false) }
+
+        it 'should return error status' do
+          get 'fhir/Patient', {}, headers
+          expect(response).to have_http_status(:error)
+        end
+
+        it 'should render OperationOutcome' do
+          get 'fhir/Patient', {}, headers
+          expect(response).to render_template('fhir_base/operation_outcome')
+          expect(response.content_type).to eq :xml
+
+        end
+      end
     end
   end
 
-  describe 'GET /fhir/Patient/1.json' do
-    it { should return_FHIR_JSON_object('Patient') }
+  describe 'GET /fhir/Patient/1' do
+    context 'JSON' do
+      it { should return_FHIR_JSON_object('Patient') }
+      it { should return_HTTP_success_for('Patient/1.json') }
+    end
 
-    it { should return_HTTP_success_for('Patient/1.json') }
+    context 'XML' do
+      let(:headers) { {'Accept' => 'application/xml'} }
 
-    it {should return_an_OperationOutcome_when_failing_for('Patient/1.json')}
+      context 'success' do
+        it 'should return a FHIR resource' do
+          get 'fhir/Patient/1', {}, headers
+          expect(response).to return_FHIR_XML_object('Patient')
+        end
+
+        it 'should have success status (2xx)' do
+          get 'fhir/Patient/1', {}, headers
+          expect(response).to have_http_status(:success)
+        end
+      end
+    end
   end
 end
