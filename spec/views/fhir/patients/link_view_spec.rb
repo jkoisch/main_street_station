9b597429c7c33e1eb9338991ebebc0a5d4222046
Dennis Rodriguez <drodriguez.0117@gmail.com>
2015-11-07 15:46:11 -0500

    XML and JSON API and View Testing of Patient Classes 
    Updated PatientClasses::Contact json view to correctly call Period
    Added support files for PatientClasses: Communication, Contact, Link
    Added view spec files for PatientClasses: Communication, Contact, Link

---
+++ spec/views/fhir/patients/link_view_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+require 'fhir/patient'
+require 'builder'
+
+describe 'FHIR View Patient Link', type: :view do
+  context 'partial' do
+    context 'simple' do
+      let(:object) { yaml_load('patients/link-standard.yaml') }
+
+      context 'json' do
+        subject { render partial: 'fhir/patients/link', formats: :json, locals: {link: object} }
+
+        it { should match_fhir_json(support_file('patients/link-standard.json')) }
+      end
+
+      context 'xml' do
+        let(:builder) { Builder::XmlMarkup.new() }
+        subject       { object.to_xml('link', builder) }
+
+        it { should match_fhir_xml(support_file('patients/link-standard.xml')) }
+      end
+    end
+
+    context 'complete' do
+      let(:object) { yaml_load('patients/link-complete.yaml') }
+
+      context 'json' do
+        subject { render partial: 'fhir/patients/link', formats: :json, locals: {link: object} }
+
+        it { should match_fhir_json(support_file('patients/link-complete.json')) }
+      end
+
+      context 'xml' do
+        let(:builder) { Builder::XmlMarkup.new() }
+        subject       { object.to_xml('link', builder) }
+
+        it { should match_fhir_xml(support_file('patients/link-complete.xml')) }
+      end
+    end
+  end
+end
