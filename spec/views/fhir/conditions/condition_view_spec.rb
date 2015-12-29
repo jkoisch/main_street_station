3ed48e1fbcdae2bd69ced95bb229725daf0c17fc
Dennis Rodriguez <drodriguez.0117@gmail.com>
2015-12-28 22:19:23 -0500

    Adding Condition Resource Show and Index View Tests
    Updating Condition view spec with additional tests for Show and Index
    Minor id changes to Condition Support Files

--- spec/views/fhir/conditions/condition_view_spec.rb
+++ spec/views/fhir/conditions/condition_view_spec.rb
@@ -38,4 +38,39 @@ describe 'FHIR Condition View', type: :view do
       it {should match_fhir_xml(support_file('conditions/condition-complete.xml')) }
     end
   end
+
+  context 'show' do
+    before(:each) { @condition = yaml_load('conditions/condition-standard.yaml') }
+
+    context 'JSON' do
+      subject { render template: 'fhir/conditions/show', formats: :json }
+
+      it { should match_fhir_json(support_file('conditions/show.json')) }
+    end
+
+    context 'XML' do
+      subject { render template: 'fhir/conditions/show', formats: :xml }
+
+      it { should match_fhir_xml(support_file('conditions/show.xml')) }
+    end
+  end
+
+  context 'index' do
+    before(:each) { @conditions = [ yaml_load('conditions/condition-standard.yaml'),
+                                   yaml_load('conditions/condition-complete.yaml') ] }
+
+    context 'JSON' do
+      subject { render template: 'fhir/conditions/index', formats: :json }
+
+      it { should match_fhir_json(support_file('conditions/index.json'),
+                                  {'**/lastUpdated' => 'xxx'}) }
+    end
+
+    context 'XML' do
+      subject { render template: 'fhir/conditions/index', formats: :xml }
+
+      it { should match_fhir_xml(support_file('conditions/index.xml'),
+                                 {"//*[local-name()='lastUpdated']/@value" => 'xxx'}) }
+    end
+  end
 end
