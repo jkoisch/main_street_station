19106c851b72897da8451a5625780cd5398dcc13
Dennis Rodriguez <drodriguez.0117@gmail.com>
2016-02-02 21:08:14 -0500

    Removed duplicate tests in family_member_history_api_spec.rb

--- spec/requests/fhir_api/family_member_history_api_spec.rb
+++ spec/requests/fhir_api/family_member_history_api_spec.rb
@@ -66,22 +66,6 @@ describe 'Family History Request FHIR API', type: :request do
       end
     end
 
-    it { should return_FHIR_JSON_bundle_object('FamilyMemberHistory') }
-
-    it 'should return_HTTP_success_for FamilyMemberHistory.json' do
-      GringottResponse.any_instance.stubs(:success?).returns(true)
-      expect(get '/fhir/FamilyMemberHistory.json'). to eq 200
-    end
-
-    it 'should return error for failure' do
-      GringottResponse.any_instance.stubs(:success?).returns(false)
-      expect(get '/fhir/FamilyMemberHistory.json').to eq 500
-    end
-
-    it 'should render OperationOutcome for a failure' do
-      GringottResponse.any_instance.stubs(:success?).returns(false)
-      expect(get '/fhir/FamilyMemberHistory.json').to render_template('fhir_base/operation_outcome')
-    end
   end
 
   describe 'GET /fhir/FamilyMemberHistory/1.json' do
